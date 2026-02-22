6a70345499c876a3426a03ee1aed73436e88ce2d
BaoT23 <tyler.bao@yahoo.com>
2026-02-22 17:50:11 -0500

    Adding Anthony's Story Generation page

--- ScrumPilot.Web/Program.cs
+++ ScrumPilot.Web/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ScrumPilot.Web;
+using ScrumPilot.Web.Services;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
