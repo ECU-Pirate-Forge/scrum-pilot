7890e6f35c0466209ca4f59bde92a1f3ee98b6b6
BaoT23 <tyler.bao@yahoo.com>
2026-02-25 19:54:39 -0500

    Added dark mode for new draft stories page

--- ScrumPilot.Web/Program.cs
+++ ScrumPilot.Web/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ScrumPilot.Web;
 using MudBlazor.Services;
+using ScrumPilot.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
