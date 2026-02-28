51810af2c269417acfa0355536cd0ea5c4b7d0b8
BaoT23 <tyler.bao@yahoo.com>
2026-02-27 21:29:51 -0500

    Removed bootstrap and most custom css that could be repliocated with MudBlazor, but ther are some bugs to sort out

--- ScrumPilot.Web/Program.cs
+++ ScrumPilot.Web/Program.cs
@@ -2,17 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ScrumPilot.Web;
 using MudBlazor.Services;
-using ScrumPilot.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Register MudBlazor services (for the button component only)
+// Register MudBlazor services with built-in theming
 builder.Services.AddMudServices();
-
-// Register simple theme service
-builder.Services.AddSingleton<IThemeService, ThemeService>();
 
 // Read ApiBaseUrl from appsettings.json
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7195/";
