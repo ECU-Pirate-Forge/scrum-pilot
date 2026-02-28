8a1ac534615196b2b3fc47cf35f161f1a49bb01a
BaoT23 <tyler.bao@yahoo.com>
2026-02-28 11:07:16 -0500

    Removed Moq from GlobalUsings

--- ScrumPilot.UnitTests/GlobalUsings.cs
+++ ScrumPilot.UnitTests/GlobalUsings.cs
@@ -1,12 +1,7 @@
 // Global using statements for the unit test project
-global using Xunit;
 global using NSubstitute;
-global using Moq;
-global using Moq.Contrib.HttpClient;
-global using Moq.Protected;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Logging;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Threading.Tasks;
+global using Xunit;
