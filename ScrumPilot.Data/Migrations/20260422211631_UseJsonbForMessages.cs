cc8f2f387f2f2bc572f4669de99bc04f6f7a1c0d
BaoT23 <tyler.bao@yahoo.com>
2026-04-22 17:22:47 -0400

    Add UseJsonbForMessages migration and update IssueLink
    Introduce empty UseJsonbForMessages EF Core migration. Update ProductBacklogItem's IssueLink property to be non-nullable in the model snapshot. No schema changes are applied yet.

---
+++ ScrumPilot.Data/Migrations/20260422211631_UseJsonbForMessages.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScrumPilot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseJsonbForMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
