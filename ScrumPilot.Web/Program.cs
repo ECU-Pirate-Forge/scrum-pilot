6c71870998da97f46b44511abff34f867c2bf4f8
BaoT23 <tyler.bao@yahoo.com>
2026-04-26 22:05:02 -0400

    Fixed Metric s ahead of Demo

--- ScrumPilot.Web/Program.cs
+++ ScrumPilot.Web/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ScrumPilot.Web;
 using ScrumPilot.Web.Auth;
+using ApexCharts;
 using ScrumPilot.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -28,6 +29,7 @@ builder.Services.AddHttpClient("API", client => client.BaseAddress = new Uri(api
     .AddHttpMessageHandler<AuthHeaderHandler>();
 builder.Services.AddScoped(sp =>
     sp.GetRequiredService<IHttpClientFactory>().CreateClient("API"));
+builder.Services.AddApexCharts();
 builder.Services.AddScoped<ScrumPilot.Web.Services.MetricsDashboardService>();
 builder.Services.AddSingleton<ScrumPilot.Web.Services.ProjectStateService>();
 
